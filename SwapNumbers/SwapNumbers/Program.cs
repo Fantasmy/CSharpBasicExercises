073fad6f2df812b9bc3e0fd76e7f129e32b484dd
Jenny <fantazmit@gmail.com>
2018-07-15 22:54:21 +0200

    Basic swaping numbers exercise

---
+++ SwapNumbers/SwapNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * C# Program to Swap two Numbers
+ */
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+
+            Console.WriteLine("\nEnter the first number: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nEnter the second number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.Write("\nAfter swaping: ");
+            Console.Write("\nFirst number: " +num1);
+            Console.Write("\nSecond number: "+num2);
+            Console.Read();
+
+        }
+    }
+}
