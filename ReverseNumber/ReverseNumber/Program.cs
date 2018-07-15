8f5f0855bfb6ae22331ca5be4f7a66f1ca44632d
Jenny <fantazmit@gmail.com>
2018-07-15 23:11:11 +0200

    Basic exercise to reverse digits from number (eg: 123 -> 321)

---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * C# Program to Get a Number and Display the Number with its Reverse
+ */
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, reverse = 0;
+
+            Console.WriteLine("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+
+            while(num != 0)
+            {
+                reverse = reverse * 10;
+                reverse = reverse + num % 10;
+                num = num / 10;
+            }
+
+            Console.WriteLine("Reverse of entered number is: "+reverse);
+            Console.ReadLine();
+        }
+    }
+}
